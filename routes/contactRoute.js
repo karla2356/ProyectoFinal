a119c255d911d30784e0bdcb22d19ee414bc8b77
karla2356 <210004@utags.edu.mx>
2024-11-30 19:32:52 -0600

    Delete routes directory

--- routes/contactRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { contactUs } = require("../controllers/contactController");
-const router = express.Router();
-const protect = require("../middleWare/authMiddlewar");
-
-router.post("/", protect, contactUs);
-
-module.exports = router;
